34e3542d3c8d308889d28e49af373de6c2e53336
chenghsuanw <chenghsuan.w@gmail.com>
2019-04-24 19:44:31 +0800

    OOP code (create Restaurant class)

---
+++ OOP/FoodPin/RestaurantDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by chenghsuan on 2019/3/24.
+//  Copyright © 2019年 chenghsuan. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+
+    @IBOutlet var restaurantImageView: UIImageView!
+    @IBOutlet var restaurantNameLabel: UILabel!
+    @IBOutlet var restaurantTypeLabel: UILabel!
+    @IBOutlet var restaurantLocationLabel: UILabel!
+    
+    var restaurant:Restaurant!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        restaurantImageView.image = UIImage(named: restaurant.image)
+        restaurantNameLabel.text = restaurant.name
+        restaurantTypeLabel.text = restaurant.type
+        restaurantLocationLabel.text = restaurant.location
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
