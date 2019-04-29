6ac4a4ad3107faa5bf4a30d397d6a05be6548124
chenghsuanw <chenghsuan.w@gmail.com>
2019-04-29 20:27:38 +0800

    start

---
+++ SelfSizingCell/FoodPin/RestaurantDetailViewController.swift
@@ -0,0 +1,77 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by chenghsuan on 2019/3/24.
+//  Copyright © 2019年 chenghsuan. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var restaurantImageView: UIImageView!
+    @IBOutlet var tableView:UITableView!
+    
+    var restaurant:Restaurant!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        restaurantImageView.image = UIImage(named: restaurant.image)
+        tableView.backgroundColor = UIColor(red: 189/255, green: 252/255, blue: 201/255, alpha: 0.5)
+        // 移除空白列的分隔線
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        // 變更分隔線顏色
+        tableView.separatorColor = UIColor(red: 127/255, green: 255/255, blue: 212/255, alpha: 0.5)
+        // 設定畫面標題
+        title = restaurant.name
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 避免滑動時隱藏navigation bar
+        navigationController?.hidesBarsOnSwipe = false
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RestaurantDetailTableViewCell
+        
+        switch indexPath.row {
+        case 0:
+            cell.fieldLabel.text = "Name"
+            cell.valueLabel.text = restaurant.name
+        case 1:
+            cell.fieldLabel.text = "Type"
+            cell.valueLabel.text = restaurant.type
+        case 2:
+            cell.fieldLabel.text = "Location"
+            cell.valueLabel.text = restaurant.location
+        case 3:
+            cell.fieldLabel.text = "Been here"
+            cell.valueLabel.text = (restaurant.isCheckIn) ? "Yes, I've been here before" : "No"
+        default:
+            cell.fieldLabel.text = ""
+            cell.valueLabel.text = ""
+        }
+        cell.backgroundColor = UIColor.clear
+        
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
