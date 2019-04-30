f81a1a047c8aef2507ed83779ed8f280023de67c
chenghsuanw <chenghsuan.w@gmail.com>
2019-04-30 11:41:45 +0800

    start

---
+++ Animation/FoodPin/Restaurant.swift
@@ -0,0 +1,26 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by chenghsuan on 2019/4/24.
+//  Copyright © 2019年 chenghsuan. All rights reserved.
+//
+
+import Foundation
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+    var phone = ""
+    var isCheckIn = false
+    
+    init(name: String, type: String, location: String, phone: String, image: String,  isCheckIn: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.phone = phone
+        self.isCheckIn = isCheckIn
+    }
+}
